47ef141a5a42a46e0df7438bf7ced73bb2d4269c
Aaron F Stanton <afstanton@gmail.com>
2022-07-04 16:42:47 -0400

    rubocop

--- app/mailers/canned_report_mailer.rb
+++ app/mailers/canned_report_mailer.rb
@@ -1,14 +1,15 @@
-class CannedReportMailer < ApplicationMailer
+# frozen_string_literal: true
 
+class CannedReportMailer < ApplicationMailer
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
   #   en.canned_report_mailer.scheduled.subject
   #
   def scheduled
-    @greeting = "Hi"
+    @greeting = 'Hi'
 
-    mail to: "to@example.org"
+    mail to: 'to@example.org'
   end
 
   # Subject can be set in your I18n file at config/locales/en.yml
@@ -17,7 +18,7 @@ class CannedReportMailer < ApplicationMailer
   #   en.canned_report_mailer.ad_hoc.subject
   #
   def ad_hoc(params:)
-    @greeting = "Hi"
+    @greeting = 'Hi'
     @report_name = params[:id].titleize
 
     report = CannedReport.new(params[:id])
@@ -25,11 +26,11 @@ class CannedReportMailer < ApplicationMailer
     results = report.run(params)
 
     tempfile = Tempfile.new(['canned_report_mailer', '.csv']).tap do |fh|
-      csv = CSV.open(fh, "wb")
-      csv << results[:results].first.keys.map { |key| key.titleize }
+      csv = CSV.open(fh, 'wb')
+      csv << results[:results].first.keys.map(&:titleize)
       results[:results].each do |result|
         row = []
-        result.each do |key, value|
+        result.each do |_key, value|
           row << value&.to_s&.gsub('"', '')
         end
 

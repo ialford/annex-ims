a74e20479ae5737a5d7d5504586cff72fd8d709f
Robert Fox <rfox2@nd.edu>
2023-02-07 16:28:20 -0500

    Flesh out health check code

--- app/jobs/health_check_job.rb
+++ app/jobs/health_check_job.rb
@@ -1,7 +1,14 @@
 class HealthCheckJob < ApplicationJob
   queue_as HealthCheckWorker::QUEUE_NAME
 
-  def perform()
-		1
+  def perform(date_time)
+    if File.exist?(Rails.root.join('tmp', 'sneakers-health-check.json'))
+      File.delete(Rails.root.join('tmp', 'sneakers-health-check.json'))
+    end
+    f = File.new(Rails.root.join('tmp', 'sneakers-health-check.json'), "w+")
+    f.puts(date_time)
+    f.close
+    true
   end
+
 end

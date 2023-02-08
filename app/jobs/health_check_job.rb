639baee8eb4005665286b6495fd6a3aa38323405
Robert Fox <rfox2@nd.edu>
2023-02-08 08:31:44 -0500

    Minor style changes

--- app/jobs/health_check_job.rb
+++ app/jobs/health_check_job.rb
@@ -5,10 +5,11 @@ class HealthCheckJob < ApplicationJob
     if File.exist?(Rails.root.join('tmp', 'sneakers-health-check.json'))
       File.delete(Rails.root.join('tmp', 'sneakers-health-check.json'))
     end
-    f = File.new(Rails.root.join('tmp', 'sneakers-health-check.json'), "w+")
+    f = File.new(Rails.root.join('tmp', 'sneakers-health-check.json'), 'w+')
     f.puts(date_time)
     f.close
     true
   end
+
 
 end

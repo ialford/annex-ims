294ab9ffafce10982e865a9ce9777fd417149f2d
Robert Fox <rfox2@nd.edu>
2023-03-25 09:20:47 -0400

    Setup for running container in development

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#!/usr/bin/bash
 set -e
 
 echo "Add github token so we can pull from private repos"
@@ -13,6 +13,7 @@ sudo -u app bundle install
 echo "Create template files"
 cp "$APP_DIR/config/secrets.yml.example" "$APP_DIR/config/secrets.yml"
 cp "$APP_DIR/config/database.yml.example" "$APP_DIR/config/database.yml"
+cp "$APP_DIR/config/sunspot.yml.example" "$APP_DIR/config/sunspot.yml"
 
 echo "Modify config file for database"
 sed -i 's/{{ database_host }}/'"$DB_HOST"'/g' "$APP_DIR/config/database.yml"
@@ -27,6 +28,9 @@ sed -i 's/{{ client_secret }}/'"$CLIENT_SECRET"'/g' "$APP_DIR/config/secrets.yml
 sed -i 's/{{ secret_key_base }}/'"$SECRET_KEY_BASE"'/g' "$APP_DIR/config/secrets.yml"
 sed -i 's/{{ host_name }}/'"$HOST_NAME"'/g' "$APP_DIR/config/secrets.yml"
 sed -i 's/{{ api_token }}/'"$API_TOKEN"'/g' "$APP_DIR/config/secrets.yml"
+if [[ "$PASSENGER_APP_ENV" == "development" ]]; then
+   sed -i 's/{{ api_host }}/'"$API_HOST"'/g' "$APP_DIR/config/secrets.yml" 
+fi
 sed -i 's/{{ coral_password }}/'"$CORAL_PASSWORD"'/g' "$APP_DIR/config/secrets.yml"
 sed -i 's/{{ service_password }}/'"$SERVICE_PASSWORD"'/g' "$APP_DIR/config/secrets.yml"
 sed -i 's/{{ refworks_password }}/'"$REFWORKS_PASSWORD"'/g' "$APP_DIR/config/secrets.yml"
@@ -56,36 +60,48 @@ echo "Fix permissions on $APP_DIR folder"
 chown -R app:app $APP_DIR
 
 if [[ "$PASSENGER_APP_ENV" == "development" ]]; then
-    echo "Adding sample bins"
-    RAILS_ENV=$PASSENGER_APP_ENV bundle exec rake sample:bins
-    echo "Adding sample shelves"
-    RAILS_ENV=$PASSENGER_APP_ENV bundle exec rake sample:shelves
-    echo "Adding sample trays"
-    RAILS_ENV=$PASSENGER_APP_ENV bundle exec rake sample:trays
-    echo "Adding sample items"
-    RAILS_ENV=$PASSENGER_APP_ENV bundle exec rake sample:items
-    echo "Adding sample users"
-    RAILS_ENV=$PASSENGER_APP_ENV bundle exec rake sample:users
+    if  [[ $RUN_TASK == 1 ]]; then  
+        echo "Run development database migrations"
+        RAILS_ENV=$PASSENGER_APP_ENV bundle exec rake db:migrate
+    fi
 fi
 
-if  [ $RUN_TASK -eq 1 ] 
-then
-    echo "Run database migrations"
-    RAILS_ENV=$PASSENGER_APP_ENV bundle exec rake db:migrate
+if [[ "$PASSENGER_APP_ENV" == "development" ]]; then
+    if  [[ $RUN_TASK == 1 ]]; then  
+        echo "Adding sample bins"
+        RAILS_ENV=$PASSENGER_APP_ENV bundle exec rake sample:bins
+        echo "Adding sample shelves"
+        RAILS_ENV=$PASSENGER_APP_ENV bundle exec rake sample:shelves
+        echo "Adding sample trays"
+        RAILS_ENV=$PASSENGER_APP_ENV bundle exec rake sample:trays
+        echo "Adding sample items"
+        RAILS_ENV=$PASSENGER_APP_ENV bundle exec rake sample:items
+        echo "Adding sample users"
+        RAILS_ENV=$PASSENGER_APP_ENV bundle exec rake sample:users
+    fi
+fi
+
+if  [[ $RUN_TASK == 1 ]]; then
+    if [[ "$PASSENGER_APP_ENV" != "development" ]]; then 
+        echo "Run database migrations"
+        RAILS_ENV=$PASSENGER_APP_ENV bundle exec rake db:migrate
+    fi
     echo "Start Passenger Service as $PASSENGER_APP_ENV"
     exec /sbin/my_init
 fi
 
-if  [ $RUN_TASK -eq 2 ]
+
+
+if  [[ $RUN_TASK == 2 ]]
 then
     echo "Start sneakers"
     RAILS_ENV=$PASSENGER_APP_ENV exec bundle exec rake sneakers:run
 fi
 
-if  [ $RUN_TASK -eq 3 ]
+if  [[ $RUN_TASK == 3 ]]
 then
     echo "Run the Scheduled Reports job once at midnight"
-    if [ $(date -u -d "now + 30 minutes" +'%H') -eq "05" ]
+    if [[ $(date -u -d "now + 30 minutes" +'%H') -eq "05" ]]
     then
         echo "Run the rails runner Scheduled Reports job"
         RAILS_ENV=$PASSENGER_APP_ENV bundle exec rake annex:run_scheduled_reports
